401b2a405f6efa9644575eb5e0d9c2482f192564
Zeynab <zsakho58@gmail.com>
2024-12-12 15:54:42 +0100

    betty fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,16 +7,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height;
-    size_t right_height;
+	size_t left_height;
+	size_t right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    return ((left_height > right_height ? left_height : right_height) + 1);
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
@@ -26,15 +26,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    size_t left_height = 0;
-    size_t right_height = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    return ((int)left_height - (int)right_height);
+	return ((int)left_height - (int)right_height);
 }
 
