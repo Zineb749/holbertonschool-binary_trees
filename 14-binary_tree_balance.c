a177d12c46d63ee7990f0344abf72ea736b6105c
Zeynab <zsakho58@gmail.com>
2024-12-12 15:33:48 +0100

    code fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,27 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures ther hight of a binary tree
- * @tree:  is a pointer to the root node of the
- * tree to measure the height.
- * Return: the number of height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	int left = 0, right = 0;
-
-	if (!tree)
-		return (0);
-	if (tree->left)
-		left = 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		right = 1 + binary_tree_height(tree->right);
-
-	return ((left > right) ? left : right);
-}
-
-
-/**
  * binary_tree_balance - check if the tree is balanced.
  * @tree: pointer to the root node of the tree to count the leaves
  *
@@ -39,4 +18,24 @@ int binary_tree_balance(const binary_tree_t *tree)
 	right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - measures ther hight of a binary tree
+ * @tree:  is a pointer to the root node of the
+ * tree to measure the height.
+ * Return: the number of height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
+
+	return ((left > right) ? left : right);
 }
